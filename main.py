f4035ab717e2ff129267c81470c06b7eb772f101
jasonbdt <ja.bladt@gmail.com>
2025-09-22 02:56:02 +0200

    scrape movie info and store data in dictionary

--- main.py
+++ main.py
@@ -15,12 +15,34 @@ def get_critics_consensus(soup: BeautifulSoup) -> str:
     }).find('p').text
 
 
+def get_movie_data(soup: BeautifulSoup) -> dict[str, Any]:
+    info_categories = soup.find('section', {
+        "class": "media-info"
+    }).find('dl').find_all('div', {
+        "class": "category-wrap"
+    })
+
+    output = {}
+    for category in info_categories:
+        info_key = category.find('rt-text', {
+            "class": "key"
+        }).text
+        info_value = category.find('dd').text.strip().split(', \n')
+        output[info_key] = info_value
+
+    return output
+
+
 def main() -> None:
     page_source = get_file_content("page.html")
     soup = BeautifulSoup(page_source, "html.parser")
 
     critics_consensus = get_critics_consensus(soup)
+    movie_data = get_movie_data(soup)
     print(critics_consensus)
+
+    for key, values in movie_data.items():
+        print(f"{key}: {", ".join(values)}")
 
 
 if __name__ == '__main__':
