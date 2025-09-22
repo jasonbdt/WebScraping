518e97233bcc1e646c3422c5a301c9aa7c9fb392
jasonbdt <ja.bladt@gmail.com>
2025-09-22 02:28:24 +0200

    Improve code styling

--- main.py
+++ main.py
@@ -9,14 +9,18 @@ def get_file_content(file_path: str) -> Any | str | None:
         print(f"File {file_path} doesn't exist!")
 
 
+def get_critics_consensus(soup: BeautifulSoup) -> str:
+    return soup.find('div', {
+        "id": "critics-consensus"
+    }).find('p').text
+
+
 def main() -> None:
     page_source = get_file_content("page.html")
     soup = BeautifulSoup(page_source, "html.parser")
 
-    critics_consensus = soup.find('div', {
-        "id": "critics-consensus"
-    }).find('p')
-    print(critics_consensus.text)
+    critics_consensus = get_critics_consensus(soup)
+    print(critics_consensus)
 
 
 if __name__ == '__main__':
